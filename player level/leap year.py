142249285698a8b2a2358643b1fc94879e5a9e4a
priyadarshinipd <37337166+priyadarshinipd@users.noreply.github.com>
2018-03-21 09:55:44 +0530

    Update leap year.py

--- player level/leap year.py
+++ player level/leap year.py
@@ -1,5 +1,5 @@
 a=int(raw_input())
 if((a%400==0)or(a%4==0)and(a%100!=0)):
-    print("it is leap year")
+    print("yes,it is leap year")
 else:
-    print("it is not leap year")
+    print("no,it is not leap year")
